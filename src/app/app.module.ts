90aca70670099ea31deaa65e4ddbdb123c2ec585
Pierre Brassart <k1888893@kcl.ac.uk>
2020-07-24 11:28:42 +0100

    updated the look of the application with materials, added scientific functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { MaterialsModule } from './materials/materials.module'
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CalculatorComponent } from './calculator/calculator.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -12,9 +15,12 @@ import { CalculatorComponent } from './calculator/calculator.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-calculator',
@@ -10,6 +11,10 @@ export class CalculatorComponent implements OnInit {
   firstOperand = null;
   operator = null;
   waitForSecondNumber = false;
+  //it's for resetting the display after using a oneNbOperation
+  //and inputting a number  
+  oneNbOperationDisplayed = false;
+  isRadianMode = true;
 
 
   constructor() { }
@@ -25,6 +30,11 @@ export class CalculatorComponent implements OnInit {
       this.currentNumber = v;
       this.waitForSecondNumber = false;
     }
+    else if (this.oneNbOperationDisplayed) {
+      this.currentNumber = v;
+      this.oneNbOperationDisplayed = false;
+      this.firstOperand = null;
+    }
     else {
       this.currentNumber === '0' ? this.currentNumber = v : this.currentNumber += v;
     }
@@ -38,7 +48,7 @@ export class CalculatorComponent implements OnInit {
     }
   }
 
-  private doCalculation(op, secondOp)
+  private doCalculation(op:string, secondOp)
   {
     switch (op){
       case '+':
@@ -49,6 +59,8 @@ export class CalculatorComponent implements OnInit {
         return this.firstOperand *= secondOp;
       case '/':
         return this.firstOperand /= secondOp;
+      case '!':
+        return this.firstOperand = factorial(this.firstOperand); 
       case '=':
         return secondOp;
     }
@@ -73,6 +85,62 @@ export class CalculatorComponent implements OnInit {
     console.log(this.firstOperand);
   }
 
+  public getOneNbOperation(op: string)
+  {
+    console.log(op);
+    if (this.firstOperand === null)
+    {
+      this.firstOperand = Number(this.currentNumber);
+    }
+    if (op === '!') {
+      const result = factorial(this.firstOperand);
+      this.currentNumber = String(result);
+      this.firstOperand = result;
+    }
+    else if (op === 'log2') {
+      const result = Math.log2(this.firstOperand);
+      this.currentNumber = String(result);
+      this.firstOperand = result;
+    }
+    else if (op === 'ln') {
+      const result = Math.log(this.firstOperand);
+      this.currentNumber = String(result);
+      this.firstOperand = result;
+    }
+    else if (op === 'sqrt') {
+      const result = Math.sqrt(this.firstOperand);
+      this.currentNumber = String(result);
+      this.firstOperand = result;
+    }
+    else if (op === 'cos') {
+      let result;
+      if (this.isRadianMode)
+      {
+        result = Math.cos(this.firstOperand);
+      }
+      else {
+        result = Math.cos(this.firstOperand * Math.PI / 180);
+      }
+      console.log(`the result is: ${result}`);
+      this.currentNumber = String(result);
+      this.firstOperand = result;
+    }
+    else if (op === 'sin') {
+      let result;
+      if (this.isRadianMode)
+      {
+        result = Math.sin(this.firstOperand);
+      }
+      else {
+        result = Math.sin(this.firstOperand * Math.PI / 180);
+      }
+      console.log(`the result is: ${result}`);
+      this.currentNumber = String(result);
+      this.firstOperand = result;
+    }
+    this.oneNbOperationDisplayed = true;
+  }
+
   public clear()
   {
     this.currentNumber = '0';
@@ -81,4 +149,28 @@ export class CalculatorComponent implements OnInit {
     this.waitForSecondNumber = false;
   }
 
+
+  colourButtonRad = 'primary';
+  colourButtonDeg = 'basic';
+  public setRadMode(bool:boolean)
+  {
+    console.log(bool);
+    this.isRadianMode = bool;
+    console.log(this.isRadianMode);
+
+    //changes the colours of the buttons
+    if (bool) 
+    {
+      this.colourButtonRad = 'primary';
+      this.colourButtonDeg = 'basic';
+    }
+    else {
+      this.colourButtonRad = 'basic';
+      this.colourButtonDeg = 'primary';
+    }
+  }
+
 }
+
+function factorial(nb:number): number {return nb > 1 ? nb * factorial(nb-1) : 1}
+
